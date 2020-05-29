6b9490b6ed9f5657906915893c627631122cbc77
Lucas Santana <lsantana@itelios.com>
2020-05-29 09:07:46 -0300

    Fixing modal popup login

---
+++ view/frontend/web/js/model/ajaxlogin-popup.js
@@ -0,0 +1,31 @@
+define([
+    'jquery',
+    'Magento_Ui/js/modal/modal'
+], function ($, modal) {
+    'use strict';
+
+    return {
+        modalWindow: null,
+
+        /**
+         * Create popUp window for provided element
+         *
+         * @param {HTMLElement} element
+         */
+        createPopUp: function (element) {
+            var options = {
+                'type': 'popup',
+                'modalClass': 'popup-authentication',
+                'focus': '[name=username]',
+                'responsive': true,
+                'innerScroll': true,
+                'trigger': '.trigger-ajax-login',
+                'buttons': []
+            };
+
+            this.modalWindow = element;
+            modal(options, $(this.modalWindow));
+
+        },
+    };
+});
